344a92b49ce9b03ca1ac08549611693cf50158c6
Wolves <cstingl@protonmail.com>
2023-12-31 11:17:47 -0500

    Add cell draw_move

--- cell.py
+++ cell.py
@@ -29,3 +29,45 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y2), Point(x1, y1))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if self._win is None:
+            return
+
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        targ_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        targ_y_mid = (to_cell._y1 + to_cell._y2) / 2
+
+        fill_clr = "red"
+        if undo:
+            fill_clr = "gray"
+        
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_clr)
+            line = Line(Point(targ_x_mid, targ_y_mid), Point(to_cell._x2, targ_y_mid))
+            self._win.draw_line(line, fill_clr)
+
+        # moving right
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_clr)
+            line = Line(Point(to_cell._x1, targ_y_mid), Point(targ_x_mid, targ_y_mid))
+            self._win.draw_line(line, fill_clr)
+
+        # moving up
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_clr)
+            line = Line(Point(targ_x_mid, to_cell._y2), Point(targ_x_mid, targ_y_mid))
+            self._win.draw_line(line, fill_clr)
+
+        # moving down
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_clr)
+            line = Line(Point(targ_x_mid, targ_y_mid), Point(targ_x_mid, to_cell._y1))
+            self._win.draw_line(line, fill_clr)
+
