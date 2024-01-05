5f170005a1efb9b440a578a74c6574981fc17395
Wolves <cstingl@protonmail.com>
2024-01-05 14:47:49 -0500

    Add mazing solving logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graphics import Window
 from maze import Maze
+import sys
 
 def main():
     num_rows = 12
@@ -9,9 +10,17 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+    sys.setrecursionlimit(9001)
     win = Window(screen_x, screen_y)
 
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 13)
+    print("Maze created...")
+    maze_solved = maze.solve()
+
+    if not maze_solved:
+        print("Maze cannot be solved.")
+    else:
+        print("Maze solved!")
 
     win.wait_for_close()
 
